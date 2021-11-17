966b4767d6dc7d98d8d97c2338bd0aeb7d1575c5
vieiramanda11 <amandavieirazevedo@gmail.com>
2021-11-16 21:21:06 -0300

    Query single pool detail

--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -23,3 +23,27 @@ export const GET_POOLS = gql`
     }
   }
 `;
+
+export const GET_POOL_DETAILS = gql`
+  query pool($id: String!) {
+    pool(id: $id) {
+      id
+      swaps {
+        amountUSD
+        id
+        timestamp
+      }
+      txCount
+      volumeUSD
+      totalValueLockedUSD
+      token0 {
+        id
+        symbol
+      }
+      token1 {
+        id
+        symbol
+      }
+    }
+  }
+`;
