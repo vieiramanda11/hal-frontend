ca49a4dc6741868b1781901ea198ff47eb8a8b2e
vieiramanda11 <amandavieirazevedo@gmail.com>
2021-11-19 21:29:03 -0300

    Create watchlist context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Pools, PoolDetails } from './pages';
+import WatchlistContextProvider from './context/WatchlistContext';
 
 const client = new ApolloClient({
   uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
@@ -11,12 +12,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/pools' element={<Pools />} />
-          <Route path='/pools/:id' element={<PoolDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <WatchlistContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/pools' element={<Pools />} />
+            <Route path='/pools/:id' element={<PoolDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </WatchlistContextProvider>
     </ApolloProvider>
   );
 }
---
+++ src/context/WatchlistContext.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export interface IToken {
+  symbol: string;
+}
+
+export interface IPool {
+  id: string;
+  txCount: string;
+  volumeUSD: string;
+  totalValueLockedUSD: string;
+  token0?: IToken;
+  token1?: IToken;
+}
+
+export type WatchlistContextState = {
+  watchlist: IPool[];
+  addPoolToWatchlist: (pool: IPool) => void;
+  removePoolFromWatchlist: (id: string) => void;
+};
+
+const contextDefaultValues: WatchlistContextState = {
+  watchlist: [],
+  addPoolToWatchlist: () => {},
+  removePoolFromWatchlist: () => {},
+};
+
+export const WatchlistContext =
+  createContext<WatchlistContextState>(contextDefaultValues);
+
+const WatchlistContextProvider = ({ children }: any) => {
+  const getLocalStorage = (key: string, initialValue: IPool[]) => {
+    try {
+      const value = localStorage.getItem(key);
+      return value ? JSON.parse(value) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  };
+
+  const [watchlist, setWatchlist] = useState<IPool[]>(() =>
+    getLocalStorage('watchlist', contextDefaultValues.watchlist)
+  );
+
+  useEffect(() => {
+    localStorage.setItem('watchlist', JSON.stringify(watchlist));
+  }, [watchlist]);
+
+  const addPoolToWatchlist = (pool: IPool) =>
+    setWatchlist((watchlist) => [...watchlist, pool]);
+
+  const removePoolFromWatchlist = (id: string) => {
+    setWatchlist(watchlist.filter((pool) => pool.id !== id));
+  };
+
+  return (
+    <WatchlistContext.Provider
+      value={{ watchlist, addPoolToWatchlist, removePoolFromWatchlist }}
+    >
+      {children}
+    </WatchlistContext.Provider>
+  );
+};
+
+export default WatchlistContextProvider;
